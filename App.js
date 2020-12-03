e70e980d64c66a7b03c3aa41719eacc4eb4a75af
pradip <dhpradeep25@gmail.com>
2020-12-03 22:27:50 +0545

    push to dev branch

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your apps v1!!!!</Text>
+      <Text>Open up App.js to start working on your apps v1 from dev branch!</Text>
       <StatusBar style="auto" />
     </View>
   );
